5b78cfe4806b2f35e7987c17fc43cf2f26b8425b
vanshuhassija <vanshuhassija@gmail.com>
2022-03-08 22:24:23 +0530

    Todo Post Operation

--- index.js
+++ index.js
@@ -3,17 +3,62 @@
 const express=require('express');
 const app=express();
 const fs=require('fs'); // File System
-
+app.use(express.json());
 // Reading a file is asynchronous
 
 
 // fs.readFile(pathOfFile,encoding,function that you want t execute after file is read)
 
-fs.readFile("dummy.json","utf-8",function(err,fileData){
-    if(err){
-        console.log("Could not read file",err);
-    }
-    else{
-        console.log(fileData);
-    }
+// fs.readFile("dummy.json","utf-8",function(err,fileData){
+//     if(err){
+//         console.log("Could not read file",err);
+//     }
+//     else{
+//         console.log(fileData);
+//     }
+// })
+
+// const data= "The data That you want to write should always be a string";
+
+// fs.writeFile("dummy2.txt",data,"utf-8",function(err){
+//     if(err){
+//         console.log("Could Not Write Data")
+//     }
+//     else{
+//         console.log("File Written Succesully")
+//     }
+
+// })
+// fs.appendFile("dummy2.txt","Append Data","utf-8",function(err){
+//     if(err){
+//         console.log("Could Not Write Data")
+//     }
+//     else{
+//         console.log("File Written Succesully")
+//     }
+
+// })
+
+// fs.rm("dummy2.txt",function(err){
+//     if(err){
+//         console.log("Could Not delete File",err);
+//     }
+//     else{
+//         console.log("File Deleted Succesfully");
+//     }
+// })
+
+app.post("/todo",function(req,res){
+    console.log("Request nody is",req.body);
+    const id=new Date().valueOf();
+    fs.writeFile(`${id}.json`,JSON.stringify(req.body),"utf-8",function(err){
+        if(err){
+            res.status(500).send("Could not write file");
+        }
+        else{
+            res.status(200).send("File Written Successfully");
+        }
+    })
 })
+
+app.listen(8080)
